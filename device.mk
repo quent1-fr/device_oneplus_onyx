dbe70d675a6af0841bfa55b2bc9d257a9fe1ba9a
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-07-14 20:29:40 +0200

    onyx: Add a KeyHandler for tri-state-key events
    Change-Id: I22a7ee5b7eec5667b14f1a587a6d0138e77dc0f3

--- device.mk
+++ device.mk
@@ -200,6 +200,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
+# KeyHandler
+PRODUCT_PACKAGES += \
+    KeyHandler
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.onyx
